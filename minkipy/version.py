810217c8cb573adc6954018950c2c46743046dc4
Martin Uhrin <martin.uhrin@gmail.com>
2020-05-20 18:46:23 +0200

    Release 0.2.4

--- minkipy/version.py
+++ minkipy/version.py
@@ -1,7 +1,7 @@
 # pylint: disable=invalid-name
 
 author_info = (('Martin Uhrin', 'martin.uhrin.10@ucl.ac.uk'),)
-version_info = (0, 2, 3)
+version_info = (0, 2, 4)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
