c933578abe60501a6ee5b794bd3c8e4d98d4b87a
Martin Uhrin <martin.uhrin@gmail.com>
2020-06-03 19:59:54 +0200

    Set kiwipy version to 0.6.0

--- setup.py
+++ setup.py
@@ -8,53 +8,52 @@ about = {}
 with open('minkipy/version.py') as f:
     exec(f.read(), about)
 
-setup(
-    name='minkipy',
-    version=about['__version__'],
-    description="Job and workflow submission made simple",
-    long_description=open('README.rst').read(),
-    url='https://github.com/muhrin/minkipy.git',
-    author='Martin Uhrin',
-    author_email='martin.uhrin.10@ucl.ac.uk',
-    license=__license__,
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    keywords='workflows schedulers',
-    install_requires=[
-        'click',
-        'mincepy[gui]>=0.12, <0.14',
-        'kiwipy[rmq]@git+https://github.com/aiidateam/kiwipy.git@5cc396229d5260d93dc6f5b6521f0d96848708ff#egg=kiwipy',
-    ],
-    extras_require={
-        'dev': [
-            'pip',
-            'pytest>4',
-            'pytest-cov',
-            'pre-commit',
-            'yapf',
-            'prospector',
-            'pylint',
-            'twine',
-        ],
-        'docs': [
-            'nbsphinx',
-            'sphinx',
-            'sphinx-autobuild',
-        ]
-    },
-    packages=[
-        'minkipy',
-        'minkipy.cli',
-    ],
-    include_package_data=True,
-    test_suite='test',
-    entry_points={
-        'console_scripts': ['minki = minkipy.cli.main:minki'],
-        'mincepy.plugins.types': ['minkipy_types = minkipy.provides:get_types',]
-    })
+setup(name='minkipy',
+      version=about['__version__'],
+      description="Job and workflow submission made simple",
+      long_description=open('README.rst').read(),
+      url='https://github.com/muhrin/minkipy.git',
+      author='Martin Uhrin',
+      author_email='martin.uhrin.10@ucl.ac.uk',
+      license=__license__,
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      keywords='workflows schedulers',
+      install_requires=[
+          'click',
+          'mincepy[gui]>=0.12, <0.14',
+          'kiwipy[rmq]~=0.6',
+      ],
+      extras_require={
+          'dev': [
+              'pip',
+              'pytest>4',
+              'pytest-cov',
+              'pre-commit',
+              'yapf',
+              'prospector',
+              'pylint',
+              'twine',
+          ],
+          'docs': [
+              'nbsphinx',
+              'sphinx',
+              'sphinx-autobuild',
+          ]
+      },
+      packages=[
+          'minkipy',
+          'minkipy.cli',
+      ],
+      include_package_data=True,
+      test_suite='test',
+      entry_points={
+          'console_scripts': ['minki = minkipy.cli.main:minki'],
+          'mincepy.plugins.types': ['minkipy_types = minkipy.provides:get_types',]
+      })
